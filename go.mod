48c51724b37548f62272badd1d2c2f7e3648f07c
Thai Tan Loi <thaitanloi365@gmail.com>
2020-08-05 18:34:24 +0700

    init project

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/thaitanloi365/go-monitor
+
+go 1.14
+
+require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/labstack/echo/v4 v4.1.16 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+
+	os.Setenv("DOCKER_HOST", "unix:///var/run/docker.sock")
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	list, err := cli.ContainerList(context.Background(), types.ContainerListOptions{
+		All: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, item := range list {
+		fmt.Println(item)
+
+	}
+
+	reader, err := cli.ContainerLogs(context.Background(), "ezielog-backend", types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+		Follow:     true,
+		Timestamps: false,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(reader)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		c.Response().WriteHeader(http.StatusOK)
+
+		// enc := json.NewDecoder(c.Response())
+		for scanner.Scan() {
+			c.Response().Write([]byte(scanner.Text()))
+			// var s = map[string]interface{}{
+			// 	"sss": scanner.Text(),
+			// }
+
+			// if err := enc.Encode(s); err != nil {
+			// 	fmt.Println(err)
+			// }
+			c.Response().Flush()
+		}
+
+		return nil
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+
+}
